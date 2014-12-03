9816dbc0ba006bf59c23b56e189dc8d7ed7d5c7f
Johan Nestaas <johannestaas@gmail.com>
2014-12-03 15:56:27 -0800

    0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name = "defang",
-    version = "0.1.0",
+    version = "0.1.2",
     description = "Defangs and refangs malicious URLs",
     author = "Johan Nestaas",
     author_email = "johannestaas@gmail.com",
