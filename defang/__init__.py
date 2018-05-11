52972a25313e2899f98f1777b940cb2122566a26
Raphaël Vinot <raphael@vinot.info>
2018-05-11 11:02:46 -0400

    fix: refang anywhere in the whole text

--- defang/__init__.py
+++ defang/__init__.py
@@ -47,13 +47,13 @@ def defanger(infile, outfile):
 
 def refang(line):
     dirty_line = re.sub(r'\[((\.)|((dot)|DOT))\]', '.', line)
-    if re.match(r'^h([x]{2}|[X]{2})p(s)*:', dirty_line):  # hXXP or hxxp
-        dirty_line = re.sub(r'^h([x]{2}|[X]{2})p', 'http', dirty_line)
-    if re.match(r'^f([x]{1}|[X]{1})p:', dirty_line):  # fXp or fxp
-        dirty_line = re.sub(r'^f([x]{1}|[X]{1})p', 'ftp', dirty_line)
+    if re.match(r'.*h([x]{2}|[X]{2})p(s)*:.*', dirty_line):  # hXXP or hxxp
+        dirty_line = re.sub(r'h([x]{2}|[X]{2})p', 'http', dirty_line)
+    if re.match(r'.*f([x]{1}|[X]{1})p:.*', dirty_line):  # fXp or fxp
+        dirty_line = re.sub(r'f([x]{1}|[X]{1})p', 'ftp', dirty_line)
     for d in other_defang_http:
-        if re.match(r'^{}:'.format(d), dirty_line):
-            dirty_line = re.sub(r'^{}'.format(d), 'http', dirty_line)
+        if re.match(r'.*{}:.*'.format(d), dirty_line):
+            dirty_line = re.sub(r'{}'.format(d), 'http', dirty_line)
     return dirty_line
 
 
